2553faf37191524cafb23a033a4365508923432f
Nelson Rodrigues <nrsjnet@gmail.com>
2020-06-01 20:09:04 -0300

    Create vonting details page

--- src/pages/Home/components/VoteCardResume.jsx
+++ src/pages/Home/components/VoteCardResume.jsx
@@ -3,7 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import { Grid, Avatar, Divider } from "@material-ui/core";
+import { Grid, Avatar, Divider, ButtonBase } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -39,60 +40,66 @@ const useStyles = makeStyles((theme) => ({
 export default function VoteCardResume(props) {
     const classes = useStyles();
     const currentPoll = props.data;
+    const history = useHistory();
+
+
+    function handleShowDetails(e) {
+        e.preventDefault();
+        history.push(`/details/${currentPoll.id}`);
+    }
+
     return (
         <Card className={classes.root}>
-            <CardContent className={classes.details}>
-                <Grid
-                    container
-                    spacing={3}
-                    alignItems={"flex-start"}
-                    direction={"column"} >
-                    <Grid xs container direction={"row"}
-                        justify={"space-between"}>
-                        <Grid item xs={8} container
-                            alignItems={"flex-start"}
-                            direction={"column"}>
-                            <Typography variant="subtitle2" align={"right"}>
-                                CLOSED | {currentPoll.timeTillDate}
-                            </Typography>
-                            <Typography variant="h6">{currentPoll.title}</Typography>
+            <ButtonBase onClick={handleShowDetails}>
+                <CardContent className={classes.details}>
+                    <Grid 
+                        container
+                        spacing={3}
+                        alignItems={"flex-start"}
+                        direction={"column"} >
+                        <Grid container direction={"row"}
+                            justify={"space-between"}>
+                            <Grid item xs={8} container
+                                alignItems={"flex-start"}
+                                direction={"column"}>
+                                <Typography variant="subtitle2" align={"right"}>
+                                    CLOSED | {currentPoll.timeTillDate}
+                                </Typography>
+                                <Typography variant="h6">{currentPoll.title}</Typography>
+                            </Grid>
+                            <Avatar className={classes.cover}
+                                variant="rounded"
+                                alt={currentPoll.title}
+                                src={currentPoll.coverImage} />
                         </Grid>
-                        <Avatar className={classes.cover}
-                            variant="rounded"
-                            alt={currentPoll.title}
-                            src={currentPoll.coverImage} />
+                        <Divider className={classes.dividerSpace} />
+                        <Grid container direction={"row"}
+                            alignItems={"center"}
+                            justify={"space-between"}>
+                            <Grid item
+                                xs={10} container
+                                direction={"row"}
+                                alignItems={"center"}>
+                                <Avatar alt={currentPoll.options[0].title}
+                                    src={currentPoll.options[0].coverImage} />
+                                <Typography
+                                    className={classes.optionsSpace}
+                                    variant="h6" color="textSecondary">
+                                    {currentPoll.options[0].title}
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={2}>
+                                <Typography
+                                    variant="h6" color="textSecondary">
+                                    {currentPoll.options[0].percentage} %
+                                </Typography>
+                            </Grid>
+
+                        </Grid>
+
                     </Grid>
-                    <Divider className={classes.dividerSpace} />
-                    <Grid container direction={"row"}
-                        alignItems={"center"}
-                        justify={"space-between"}>
-                        <Grid item
-                            xs={10} container
-                            direction={"row"}
-                            alignItems={"center"}>
-                            <Avatar alt={currentPoll.options[0].title}
-                                src={currentPoll.options[0].coverImage} />
-                            <Typography
-                                className={classes.optionsSpace}
-                                variant="h6" color="textSecondary">
-                                {currentPoll.options[0].title}
-                            </Typography>
-                        </Grid>
-                        <Grid
-                            item
-                            xs={2}
-                            alignItems={"center"}>
-
-                            <Typography
-                                variant="h6" color="textSecondary">
-                                {currentPoll.options[0].percentage} %
-                            </Typography>
-                        </Grid>
-
-                    </Grid>
-
-                </Grid>
-            </CardContent>
+                </CardContent>
+            </ButtonBase>
         </Card>
     );
 }
